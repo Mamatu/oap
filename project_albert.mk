2de89aaab4dc7f02151b55ee449550d17fc16c82
M.Matula <mmatula@mmatula-R580-R590.(none)>
2014-07-18 21:54:13 +0200

    OGLA-1 Added two configuration projects files.

--- project_albert.mk
+++ project_albert.mk
@@ -1,3 +1,5 @@
 CUDA_PATH := /usr/local/cuda-6.0
 MODE := Debug
 PLATFORM := albert
+EXTRA_CXXOPTIONS :=
+EXTRA_NVCCOPTIONS := -arch=sm_30
---
+++ project_samsung.mk
@@ -0,0 +1,5 @@
+CUDA_PATH := /usr/local/cuda-6.0
+MODE := Debug
+PLATFORM := lcoal
+EXTRA_CXXOPTIONS :=
+EXTRA_NVCCOPTIONS :=
