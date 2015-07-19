48eb3a3c0bbf3232780d35bcde88c75e6f95ecb8
Marcin Matula <marcin.matula@gmail.com>
2015-07-19 11:33:30 +0200

    OGLA-1 Deleted oglaMatrixCpuTest folder.

--- oglaMatrixCpuTest/module.mk
+++
@@ -1,8 +0,0 @@
-include ../project_generic.mk
-TARGET := oglamatrixcputest
-INCLUDE_PATHS := 
-EXTRA_LIBS := $(OGLA_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboglaMatrix.so \
-	$(OGLA_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboglaMatrixCpu.so \
-	$(OGLA_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboglaMath.so \
-	$(OGLA_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboglaUtils.so \
-	$(OGLA_PATH)/dist/$(MODE)/$(PLATFORM)/lib/libArnoldiPackage.so 
