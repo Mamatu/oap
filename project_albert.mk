443cb6b3b5ae28163345a4953d881fa1343ba46e
Marcin Matula <marcin.matula@gmail.com>
2015-08-16 23:39:14 +0200

    OGLA-1 Added verbose mode for ptx.

--- project_albert.mk
+++ project_albert.mk
@@ -2,6 +2,6 @@ CUDA_PATH := /usr/local/cuda-6.0
 MODE := Debug
 PLATFORM := albert
 EXTRA_CXXOPTIONS :=
-EXTRA_NVCCOPTIONS := -arch=sm_30
+EXTRA_NVCCOPTIONS := -arch=sm_30 --ptxas-options --verbose
 CXX := g++-4.4
 NVCC := $(CUDA_PATH)/bin/nvcc --compiler-bindir /usr/bin/gcc-4.8
--- project_lenovo.mk
+++ project_lenovo.mk
@@ -2,6 +2,6 @@ CUDA_PATH := /usr/local/cuda-6.0
 MODE := Debug
 PLATFORM := lenovo
 EXTRA_CXXOPTIONS :=
-EXTRA_NVCCOPTIONS := -arch=sm_30
+EXTRA_NVCCOPTIONS := -arch=sm_30 --ptxas-options --verbose 
 CXX := g++
 NVCC := $(CUDA_PATH)/bin/nvcc --compiler-bindir /usr/bin/gcc-4.8
