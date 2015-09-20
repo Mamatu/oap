2f328f34dde5f81ae6590fbd4f4b4da6df15e8fc
Marcin Matula <marcin.matula@gmail.com>
2015-09-20 22:26:31 +0200

    OAP-1 UT for device matrix module.

---
+++ oglaTestsDevice/oglaDeviceMatrixModuleTests.cpp
@@ -0,0 +1,32 @@
+#include "DeviceMatrixModules.h"
+#include "KernelExecutor.h"
+#include "gtest/gtest.h"
+
+class OglaDeviceMatrixModuleTests : public testing::Test {
+ public:
+  virtual void SetUp() { device::Context::Instance().create(); }
+
+  virtual void TearDown() { device::Context::Instance().destroy(); }
+};
+
+TEST_F(OglaDeviceMatrixModuleTests, GetColumnsTest) {
+  uintt columns = 15;
+  uintt rows = 10;
+  math::Matrix* matrix = device::NewDeviceMatrix(true, true, columns, rows);
+  uintt expected = CudaUtils::GetColumns(matrix);
+  uintt tested = device::GetColumns(matrix);
+  EXPECT_EQ(expected, tested);
+  EXPECT_EQ(columns, tested);
+  device::DeleteDeviceMatrix(matrix);
+}
+
+TEST_F(OglaDeviceMatrixModuleTests, GetRowsTest) {
+  uintt columns = 15;
+  uintt rows = 10;
+  math::Matrix* matrix = device::NewDeviceMatrix(true, true, columns, rows);
+  uintt expected = CudaUtils::GetRows(matrix);
+  uintt tested = device::GetRows(matrix);
+  EXPECT_EQ(expected, tested);
+  EXPECT_EQ(rows, tested);
+  device::DeleteDeviceMatrix(matrix);
+}
