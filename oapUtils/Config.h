fb4861842a88ce9484112b08e1d50c504eb0bea0
Marcin Matula <marcin.matula@gmail.com>
2016-11-06 21:36:14 +0100

    OAP-1 Config class - absolute path to data in oap project.

---
+++ oapUtils/Config.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2016 Marcin Matula
+ *
+ * This file is part of Oap.
+ *
+ * Oap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Oap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Oap.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+
+namespace utils {
+class Config {
+ public:
+  static const std::string& getOapPath();
+
+  static std::string getPathInOap(const char* relativePath);
+
+ private:
+  static std::string oapPath;
+};
+}
+
+#endif  // CONFIG_H
