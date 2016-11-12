2872db414fbf5ebdb9c3c72034bdba8cde42bfc9
Marcin Matula <marcin.matula@gmail.com>
2016-11-12 12:43:10 +0100

    OAP-1 oap2dt3d Update of module.mk files in oap2dt3d and oap2dt3dFuncTests.

--- oap2dt3d/module.mk
+++ oap2dt3d/module.mk
@@ -1,4 +1,8 @@
 include ../project_generic.mk
 TARGET := oap2dt3d
 INCLUDE_PATHS :=
-EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboap2dt3dUtils.so
+EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboap2dt3dUtils.so \
+              $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMatrixCpu.so \
+              $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMatrix.so \
+              $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapUtils.so
+
