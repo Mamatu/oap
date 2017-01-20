44876ed22918996bf0ee8156c4919deea7fe8418
Marcin Matula <marcin.matula@gmail.com>
2017-01-21 00:59:24 +0100

    OAP-1 oap2dt3d_development Correct name of oap2dt3dDevice output.

--- oap2dt3dDevice/module.mk
+++ oap2dt3dDevice/module.mk
@@ -1,5 +1,5 @@
 include ../project_generic.mk
-TARGET := liboap2dt3dUtils
+TARGET := liboap2dt3dDevice
 INCLUDE_PATHS :=
 EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/libArnoldiPackage.so
 EXTRA_CXXOPTIONS := -std=c++11
