9d883fdb33023314002893d258a6f49ee6667b2e
Marcin Matula <marcin.matula@gmail.com>
2017-03-11 21:19:05 +0100

    OAP-1 MatrixAPI realRows and realColumns are not used in matrix api.

--- oapMatrix/MatrixAPI.h
+++ oapMatrix/MatrixAPI.h
@@ -65,36 +65,36 @@ __hostdeviceinline__ void Pop(math::Matrix* m) {}
 __hostdeviceinline__ void SetRe(math::Matrix* m, uintt c, uintt r, floatt v) {
   m->reValues[c + r * m->columns] = v;
   test::setRe(m, c, r, v);
-  assert(c + r * m->columns < m->realRows * m->realColumns);
+  assert(c + r * m->columns < m->rows * m->columns);
 }
 
 __hostdeviceinline__ floatt GetRe(const math::Matrix* m, uintt c, uintt r) {
   test::getRe(m, c, r, m->reValues[c + r * m->columns]);
-  assert(c + r * m->columns < m->realRows * m->realColumns);
+  assert(c + r * m->columns < m->rows * m->columns);
   return m->reValues[c + r * m->columns];
 }
 
 __hostdeviceinline__ floatt GetReIndex(const math::Matrix* m, uintt index) {
   test::getRe(m, index, m->reValues[index]);
-  assert(index < m->realRows * m->realColumns);
+  assert(index < m->rows * m->columns);
   return m->reValues[index];
 }
 
 __hostdeviceinline__ void SetIm(math::Matrix* m, uintt c, uintt r, floatt v) {
   m->imValues[c + r * m->columns] = v;
   test::setIm(m, c, r, v);
-  assert(c + r * m->columns < m->realRows * m->realColumns);
+  assert(c + r * m->columns < m->rows * m->columns);
 }
 
 __hostdeviceinline__ floatt GetIm(const math::Matrix* m, uintt c, uintt r) {
   test::getIm(m, c, r, m->imValues[c + r * m->columns]);
-  assert(c + r * m->columns < m->realRows * m->realColumns);
+  assert(c + r * m->columns < m->rows * m->columns);
   return m->imValues[c + r * m->columns];
 }
 
 __hostdeviceinline__ floatt GetImIndex(const math::Matrix* m, uintt index) {
   test::getIm(m, index, m->imValues[index]);
-  assert(index < m->realRows * m->realColumns);
+  assert(index < m->rows * m->columns);
   return m->imValues[index];
 }
 
