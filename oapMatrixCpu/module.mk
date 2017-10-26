3304e997ad08e143a8170c3218a6ad5252c30d52
Marcin Matula <marcin.matula@gmail.com>
2017-10-27 00:09:07 +0200

    OAP-1 oapMatrixCpu - c++11 flag.

--- oapMatrixCpu/module.mk
+++ oapMatrixCpu/module.mk
@@ -2,4 +2,5 @@ include ../project_generic.mk
 TARGET := liboapMatrixCpu
 INCLUDE_PATHS :=
 EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMath.so\
-	$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapUtils.so \
+	$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapUtils.so
+EXTRA_CXXOPTIONS := -std=c++11
