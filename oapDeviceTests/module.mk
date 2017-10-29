01f31ddb9f8237f3c29598df821d2bef387a7661
Marcin Matula <marcin.matula@gmail.com>
2017-10-29 21:53:26 +0100

    OAP-1 Added utils lib to oapDeviceTests.

--- oapDeviceTests/module.mk
+++ oapDeviceTests/module.mk
@@ -1,5 +1,5 @@
 include ../project_generic.mk
 TARGET := oapDeviceTests
 INCLUDE_PATHS :=
-EXTRA_LIBS :=
+EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapUtils.so
 EXTRA_CXXOPTIONS := -std=c++11
