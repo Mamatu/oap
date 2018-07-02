ea906a994c9375ee816dd7cf1104fb7faea6a30d
Marcin Matula <marcin.matula@gmail.com>
2018-07-03 00:15:45 +0200

    oapNeural - initial commit of application

--- oapNeural/main.cpp
+++ oapNeural/main.cpp
@@ -26,8 +26,12 @@
 
 #include "KernelExecutor.h"
 #include "CuProceduresApi.h"
+#include "oapHostMatrixUPtr.h"
 
 #include <vector>
+#include <cmath>
+#include <utility>
+#include <random>
 
 class Layer
 {
@@ -35,6 +39,7 @@ class Layer
     math::Matrix* m_inputs;
     math::Matrix* m_weights;
     size_t m_neuronsCount;
+    std::pair<size_t, size_t> m_weightsDim;
   
     Layer() : m_inputs(nullptr), m_weights(nullptr), m_neuronsCount(0)
     {}
@@ -53,6 +58,7 @@ class Layer
     void allocateWeights(const Layer* nextLayer)
     {
       m_weights = oap::cuda::NewDeviceMatrix (m_neuronsCount, nextLayer->m_neuronsCount);
+      m_weightsDim = std::make_pair(m_neuronsCount, nextLayer->m_neuronsCount);
     }
 
     void deallocate()
@@ -79,11 +85,46 @@ class Layer
     {
       oap::cuda::CopyDeviceMatrixToDeviceMatrix (m_weights, weights);
     }
+
+    static std::unique_ptr<math::Matrix, std::function<void(const math::Matrix*)>> createRandomMatrix(size_t columns, size_t rows)
+    {
+      std::unique_ptr<math::Matrix, std::function<void(const math::Matrix*)>> randomMatrix(oap::host::NewReMatrix(columns, rows),
+                      [](const math::Matrix* m){oap::host::DeleteMatrix(m);});
+
+
+      std::random_device rd;
+      std::default_random_engine dre (rd());
+      std::uniform_real_distribution<> dis(0., 1.);
+
+      for (size_t idx = 0; idx < columns*rows; ++idx)
+      {
+        randomMatrix->reValues[idx] = dis(dre);
+      }
+
+      return std::move (randomMatrix);
+    }
+
+    void initRandomWeights()
+    {
+      if (m_weights == nullptr)
+      {
+        throw std::runtime_error("m_weights == nullptr");
+      }
+
+      auto randomMatrix = createRandomMatrix (m_weightsDim.first, m_weightsDim.second);
+      oap::cuda::CopyHostMatrixToDeviceMatrix (m_weights, randomMatrix.get());
+    }
 };
 
 class Network
 {
+    oap::CuProceduresApi m_cpApi;
   public:
+    static floatt sigma(floatt value)
+    {
+      return 1.f / (1.f + std::exp(-value));
+    }
+
     Network()
     {}
 
@@ -95,8 +136,6 @@ class Network
     Layer* createLayer(size_t neurons)
     {
       Layer* layer = new Layer();
-      layer->allocateNeurons (neurons);
-      m_layers.push_back (layer);
 
       Layer* previous = nullptr;
       
@@ -105,25 +144,51 @@ class Network
         previous = m_layers.back();
       }
       
+      layer->allocateNeurons (neurons);
+      m_layers.push_back (layer);
+
       if (previous != nullptr)
       {
         previous->allocateWeights (layer);
       }
  
+      return layer;
     }
 
-    void runHostArgs (math::Matrix* hostInputs, math::Matrix* expectedHostOutputs)
+    void runHostArgsTest (math::Matrix* hostInputs, math::Matrix* expectedHostOutputs)
     {
       Layer* layer = m_layers.front();
 
       oap::cuda::CopyHostMatrixToDeviceMatrix (layer->m_inputs, hostInputs);
+
+      executeAlgo(false);
     }
 
-    void runDeviceArgs (math::Matrix* hostInputs, math::Matrix* expectedDeviceOutputs)
+    void runDeviceArgsTest (math::Matrix* hostInputs, math::Matrix* expectedDeviceOutputs)
     {
       Layer* layer = m_layers.front();
 
       oap::cuda::CopyDeviceMatrixToDeviceMatrix (layer->m_inputs, hostInputs);
+
+      executeAlgo(false);
+    }
+
+    oap::HostMatrixUPtr runHostArgs (math::Matrix* hostInputs)
+    {
+      Layer* layer = m_layers.front();
+
+      oap::cuda::CopyHostMatrixToDeviceMatrix (layer->m_inputs, hostInputs);
+
+      return executeAlgo(true);
+    }
+
+    oap::HostMatrixUPtr runDeviceArgs (math::Matrix* hostInputs)
+    {
+      Layer* layer = m_layers.front();
+
+      oap::cuda::CopyDeviceMatrixToDeviceMatrix (layer->m_inputs, hostInputs);
+
+      return executeAlgo(true);
     }
 
     void setHostWeights (math::Matrix* weights, size_t layerIndex = 0)
@@ -149,7 +214,49 @@ class Network
       }
       m_layers.clear();
     }
+
+    oap::HostMatrixUPtr executeAlgo(bool mode)
+    {
+      if (m_layers.size() < 2)
+      {
+        throw std::runtime_error ("m_layers.size() is lower than 2");
+      }
+
+      Layer* previous = nullptr;
+      Layer* current = m_layers[0];
+ 
+      for (size_t idx = 1; idx < m_layers.size(); ++idx)
+      {
+        previous = current;
+
+        current = m_layers[idx];
+
+        m_cpApi.dotProduct (current->m_inputs, previous->m_weights, previous->m_inputs);
+        m_cpApi.sigmoid(current->m_inputs);
+      }
+
+      if (mode)
+      {
+        auto llayer = m_layers.back();
+        math::Matrix* output = oap::host::NewMatrix(1, llayer->m_neuronsCount);
+        oap::cuda::CopyDeviceMatrixToHostMatrix (output, llayer->m_inputs);
+        return oap::HostMatrixUPtr (output);
+      }
+
+      return oap::HostMatrixUPtr(nullptr);
+    }
 };
+
+Network* createNetwork(size_t width, size_t height)
+{
+  Network* network = new Network();
+
+  network->createLayer(width*height);
+  network->createLayer(15);
+  network->createLayer(10);
+
+  return network;
+}
 
 int main()
 {
@@ -178,12 +285,9 @@ int main()
   };
   oap::cuda::Context::Instance().create();
 
-  Network* network = new Network();
+  Network* network = nullptr;
 
-  network->createLayer(320*200);
-  network->createLayer(15);
-  network->createLayer(10);
-  std::string dataPath = utils::Config::getPathInOap("oap2dt3d/data/");
+  std::string dataPath = utils::Config::getPathInOap("oapNeural/data/");
   dataPath = dataPath + "digits/";
 
   for (size_t idx = 0; idx < dataSet.size(); ++idx)
@@ -195,21 +299,29 @@ int main()
 
     size_t width = png.getOutputWidth().optSize;
     size_t height = png.getOutputHeight().optSize;
-    
+
+    if (idx == 0)
+    {
+      network = createNetwork (width, height);
+    }
+
     math::Matrix* input = oap::host::NewReMatrix (width, height);
-    math::Matrix* output = oap::host::NewReMatrix (1, 10);
+    math::Matrix* eoutput = oap::host::NewReMatrix (1, 10);
     png.getFloattVector(input->reValues); 
 
     if (pair.second > -1)
     {
-      output->reValues[pair.second] = 1;
+      eoutput->reValues[pair.second] = 1;
     }
 
-    network->runHostArgs (input, output);
+    network->runHostArgsTest (input, eoutput);
 
     oap::host::DeleteMatrix (input);
-    oap::host::DeleteMatrix (output);
+    oap::host::DeleteMatrix (eoutput);
   }
+
+  //auto output = network->runHostArgs (input);
+  //oap::host::PrintMatrix (output.get ());
 
   delete network;
   oap::cuda::Context::Instance().destroy();
--- oapNeural/module.mk
+++ oapNeural/module.mk
@@ -1,12 +1,10 @@
 include ../project_generic.mk
 
-$(shell tar -xzf ./data/images_monkey.tar.gz -C ./data)
-$(shell tar -xzf ./data/images_monkey_125.tar.gz -C ./data)
-
 TARGET := oapNeural
 INCLUDE_PATHS :=
 EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapAppUtils.so \
 		$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboap2dt3dDevice.so \
+		$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMatrixCuda.so \
 		$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMatrixCpu.so \
 		$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapMatrix.so \
 		$(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapUtils.so
