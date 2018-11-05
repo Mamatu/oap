822913a201fe720e45e888e83c180cd91ef2f354
Marcin Matula <marcin.matula@gmail.com>
2018-11-06 00:31:23 +0100

    c++11 in oapMatrix. MatrixInfo - added data about size in memory

--- oapMatrix/MatrixInfo.h
+++ oapMatrix/MatrixInfo.h
@@ -3,13 +3,30 @@
 
 #include "Matrix.h"
 #include <sstream>
+#include <utility>
 #include <cstddef>
+
+#include <limits>
 
 namespace math
 {
 
-class MatrixInfo {
+class MatrixInfo
+{
+ int toInt (bool b) const
+ {
+   return b ? 1 : 0;
+ }
+
  public:
+  enum Units
+  {
+    B = 0,
+    KB = 1,
+    MB,
+    GB
+  };
+
   inline MatrixInfo() : isRe(false), isIm(false) {
     m_matrixDim.columns = 0;
     m_matrixDim.rows = 0;
@@ -21,7 +38,7 @@ class MatrixInfo {
     m_matrixDim.rows = _rows;
   }
 
-  inline MatrixInfo(math::Matrix* hostMatrix) {
+  explicit inline MatrixInfo(math::Matrix* hostMatrix) {
     isRe = hostMatrix->reValues != NULL;
     isIm = hostMatrix->imValues != NULL;
 
@@ -59,8 +76,66 @@ class MatrixInfo {
   std::string toString() const
   {
     std::stringstream stream;
-    stream << "(" << isRe << ", " << isIm << ", " << m_matrixDim.columns << ", " << m_matrixDim.rows << ")";
+    std::pair<size_t, Units> size = getSize();
+    stream << "(" << isRe << ", " << isIm << ", " << m_matrixDim.columns << ", " << m_matrixDim.rows << ", " << size.first << " " << toString(size.second) << ")";
     return stream.str();
+  }
+
+  std::string toString(Units units) const
+  {
+    if (units == B) { return "B"; }
+    if (units == KB) { return "KiB"; }
+    if (units == MB) { return "MiB"; }
+    if (units == GB) { return "GiB"; }
+    return "";
+  }
+
+  std::pair<size_t, Units> getSize () const
+  {
+    Units units = B;
+    size_t size = getSizeInBytes ();
+
+    uintt tsize = getSizeInKB ();
+    if (tsize > 0)
+    {
+      size = tsize; units = KB;
+    }
+
+    tsize = getSizeInMB ();
+    if (tsize > 0)
+    {
+      size = tsize; units = MB;
+    }
+
+    tsize = getSizeInGB ();
+    if (tsize > 0)
+    {
+      size = tsize; units = GB;
+    }
+
+    return std::make_pair(size, units);
+  }
+
+  size_t getSizeInBytes () const
+  {
+    size_t size1 = m_matrixDim.columns * m_matrixDim.rows;
+    size_t size = (toInt (isRe) + toInt (isIm)) * size1 * sizeof(floatt);
+    return size;
+  }
+
+  size_t getSizeInKB () const
+  {
+    return getSizeInBytes () / 1024;
+  }
+
+  size_t getSizeInMB () const
+  {
+    return getSizeInKB () / (1024);
+  }
+
+  size_t getSizeInGB () const
+  {
+    return getSizeInMB () / (1024);
   }
 
   math::MatrixDim m_matrixDim;
--- oapMatrix/module.mk
+++ oapMatrix/module.mk
@@ -1,3 +1,5 @@
 include ../project_generic.mk
 TARGET := liboapMatrix
 INCLUDE_PATHS :=
+EXTRA_CXXOPTIONS := -std=c++11
+
