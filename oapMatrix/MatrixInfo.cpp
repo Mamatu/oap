981c249a01c2ff849e8b4454c757f11f4fb9afd2
Marcin Matula <marcin.matula@gmail.com>
2018-11-11 18:48:43 +0100

    MatrixInfo - bugfix: caclcuation of size for large values. Created MatrixInfo.cpp

---
+++ oapMatrix/MatrixInfo.cpp
@@ -0,0 +1,119 @@
+#include "MatrixInfo.h"
+
+namespace math
+{
+
+int MatrixInfo::toInt (bool b) const
+{
+  return b ? 1 : 0;
+}
+
+
+MatrixInfo::MatrixInfo() : isRe(false), isIm(false)
+{
+  m_matrixDim.columns = 0;
+  m_matrixDim.rows = 0;
+}
+
+MatrixInfo::MatrixInfo(bool _isRe, bool _isIm, uintt _columns, uintt _rows) : isRe(_isRe), isIm(_isIm)
+{
+  m_matrixDim.columns = _columns;
+  m_matrixDim.rows = _rows;
+}
+
+MatrixInfo::MatrixInfo(math::Matrix* hostMatrix)
+{
+  isRe = hostMatrix->reValues != NULL;
+  isIm = hostMatrix->imValues != NULL;
+
+  m_matrixDim.columns = hostMatrix->columns;
+  m_matrixDim.rows = hostMatrix->rows;
+}
+
+bool MatrixInfo::isInitialized() const
+{
+  return !(!isRe && !isIm);
+}
+
+void MatrixInfo::deinitialize ()
+{
+  isRe = false;
+  isIm = false;
+}
+
+bool MatrixInfo::operator==(const MatrixInfo& mInfo) const
+{
+  return isRe == mInfo.isRe && isIm == mInfo.isIm &&
+         m_matrixDim.columns == mInfo.m_matrixDim.columns && m_matrixDim.rows == mInfo.m_matrixDim.rows;
+}
+
+bool MatrixInfo::operator!=(const MatrixInfo& minfo) const
+{
+  return !(*this == minfo);
+}
+
+MatrixInfo::operator bool() const
+{
+  return isInitialized ();
+}
+
+std::string MatrixInfo::toString() const
+{
+  std::stringstream stream;
+  std::pair<size_t, Units> size = getSize();
+  stream << "(" << isRe << ", " << isIm << ", " << m_matrixDim.columns << ", " << m_matrixDim.rows << ", " << size.first << " " << toString(size.second) << ")";
+  return stream.str();
+}
+
+std::string MatrixInfo::toString(Units units) const
+{
+  if (units == B) { return "B"; }
+  if (units == KB) { return "KiB"; }
+  if (units == MB) { return "MiB"; }
+  if (units == GB) { return "GiB"; }
+  return "";
+}
+
+std::array<size_t, 4> MatrixInfo::getSizeInBuffers() const
+{
+  std::array<size_t, 4> sizes {0, 0, 0, 0};
+
+  size_t s1 = 0;
+  size_t s2 = 0;
+
+  if (m_matrixDim.columns <= m_matrixDim.rows)
+  {
+    s1 = m_matrixDim.rows;
+    s2 = m_matrixDim.columns;
+  }
+  else
+  {
+    s1 = m_matrixDim.columns;
+    s2 = m_matrixDim.rows;
+  }
+
+  for (int idx = 3; idx >= 0; --idx)
+  {
+    floatt size = static_cast<floatt>(s1) / pow(1024, idx);
+    size = size * s2;
+    sizes[idx] = size * (toInt (isRe) + toInt (isIm)) * sizeof(floatt);
+  }
+  return sizes;
+}
+
+std::pair<size_t, MatrixInfo::Units> MatrixInfo::getSize () const
+{
+  std::array<size_t, 4> sizes = getSizeInBuffers ();
+
+  for (int idx = 3; idx >= 0; --idx)
+  {
+    Units units = static_cast<Units>(idx);
+    size_t rsize = sizes[units];
+    if (rsize > 0)
+    {
+      return std::make_pair(rsize, units);
+    }
+  }
+  return std::make_pair(0, KB);
+}
+}
--- oapMatrix/MatrixInfo.h
+++ oapMatrix/MatrixInfo.h
@@ -6,17 +6,16 @@
 #include <utility>
 #include <cstddef>
 
+#include <array>
 #include <limits>
+#include <cmath>
 
 namespace math
 {
 
 class MatrixInfo
 {
- int toInt (bool b) const
- {
-   return b ? 1 : 0;
- }
+ int toInt (bool b) const;
 
  public:
   enum Units
@@ -27,116 +26,29 @@ class MatrixInfo
     GB
   };
 
-  inline MatrixInfo() : isRe(false), isIm(false) {
-    m_matrixDim.columns = 0;
-    m_matrixDim.rows = 0;
-  }
+  MatrixInfo();
 
-  inline MatrixInfo(bool _isRe, bool _isIm, uintt _columns, uintt _rows)
-      : isRe(_isRe), isIm(_isIm) {
-    m_matrixDim.columns = _columns;
-    m_matrixDim.rows = _rows;
-  }
+  MatrixInfo(bool _isRe, bool _isIm, uintt _columns, uintt _rows);
 
-  explicit inline MatrixInfo(math::Matrix* hostMatrix) {
-    isRe = hostMatrix->reValues != NULL;
-    isIm = hostMatrix->imValues != NULL;
+  explicit MatrixInfo (math::Matrix* hostMatrix);
 
-    m_matrixDim.columns = hostMatrix->columns;
-    m_matrixDim.rows = hostMatrix->rows;
-  }
+  bool isInitialized() const;
 
-  bool isInitialized() const
-  {
-    return !(!isRe && !isIm);
-  }
+  void deinitialize ();
 
-  void deinitialize ()
-  {
-    isRe = false;
-    isIm = false;
-  }
+  bool operator==(const MatrixInfo& mInfo) const;
 
-  bool operator==(const MatrixInfo& mInfo) const
-  {
-    return isRe == mInfo.isRe && isIm == mInfo.isIm &&
-           m_matrixDim.columns == mInfo.m_matrixDim.columns && m_matrixDim.rows == mInfo.m_matrixDim.rows;
-  }
+  bool operator!=(const MatrixInfo& minfo) const;
 
-  bool operator!=(const MatrixInfo& minfo) const
-  {
-    return !(*this == minfo);
-  }
+  operator bool() const;
 
-  operator bool() const
-  {
-    return isInitialized ();
-  }
+  std::string toString() const;
 
-  std::string toString() const
-  {
-    std::stringstream stream;
-    std::pair<size_t, Units> size = getSize();
-    stream << "(" << isRe << ", " << isIm << ", " << m_matrixDim.columns << ", " << m_matrixDim.rows << ", " << size.first << " " << toString(size.second) << ")";
-    return stream.str();
-  }
+  std::string toString(Units units) const;
 
-  std::string toString(Units units) const
-  {
-    if (units == B) { return "B"; }
-    if (units == KB) { return "KiB"; }
-    if (units == MB) { return "MiB"; }
-    if (units == GB) { return "GiB"; }
-    return "";
-  }
+  std::array<size_t, 4> getSizeInBuffers() const;
 
-  std::pair<size_t, Units> getSize () const
-  {
-    Units units = B;
-    size_t size = getSizeInBytes ();
-
-    uintt tsize = getSizeInKB ();
-    if (tsize > 0)
-    {
-      size = tsize; units = KB;
-    }
-
-    tsize = getSizeInMB ();
-    if (tsize > 0)
-    {
-      size = tsize; units = MB;
-    }
-
-    tsize = getSizeInGB ();
-    if (tsize > 0)
-    {
-      size = tsize; units = GB;
-    }
-
-    return std::make_pair(size, units);
-  }
-
-  size_t getSizeInBytes () const
-  {
-    size_t size1 = m_matrixDim.columns * m_matrixDim.rows;
-    size_t size = (toInt (isRe) + toInt (isIm)) * size1 * sizeof(floatt);
-    return size;
-  }
-
-  size_t getSizeInKB () const
-  {
-    return getSizeInBytes () / 1024;
-  }
-
-  size_t getSizeInMB () const
-  {
-    return getSizeInKB () / (1024);
-  }
-
-  size_t getSizeInGB () const
-  {
-    return getSizeInMB () / (1024);
-  }
+  std::pair<size_t, Units> getSize () const;
 
   math::MatrixDim m_matrixDim;
   bool isRe;
