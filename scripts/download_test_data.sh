3991b08a4829f8dc18d58da4204959401f423e4b
Marcin Matula <marcin.matula@gmail.com>
2019-02-06 23:03:44 +0100

    download_test_data new data package: oap_test_data_06_02_2019

--- scripts/download_test_data.sh
+++ scripts/download_test_data.sh
@@ -3,19 +3,21 @@
 DIR1=../oap2dt3d/data
 DIR2=../oap2dt3dFuncTests/data
 DIR3=../oapDeviceTests/data
+DIR4=../oapNeural/data
 
 URL=https://sourceforge.net/projects/openap/files/oap_test_data
-DATE=08_03_2018
+DATE=06_02_2019
 ARCHIVE_NAME=oap_test_data_$DATE.tar.gz
 
 if [ "$1" == "preclean" ]; then
   rm -r $DIR1/*
   rm -r $DIR2/*
   rm -r $DIR3/*
+  rm -r $DIR4/*
 fi
 
-if [ "$(ls -A $DIR1)" -o "$(ls -A $DIR2)" -o "$(ls -A $DIR3)" ] ; then
-  echo "Dirs oap2dt3d/data, oap2dt3dFuncTests/data, oapArnoldiDeviceTests/data must be empty for this operation."
+if [ "$(ls -A $DIR1)" -o "$(ls -A $DIR2)" -o "$(ls -A $DIR3)" -o "$(ls -A $DIR4)" ] ; then
+  echo "Dirs oap2dt3d/data, oap2dt3dFuncTests/data, oapArnoldiDeviceTests/data oapNeural/data must be empty for this operation."
 else
   wget $URL/$ARCHIVE_NAME
   tar -xvf $ARCHIVE_NAME -C .. --strip-components=1
