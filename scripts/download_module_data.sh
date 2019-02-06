d3c1e31c93d4a32d0a2f217bd29afcc687667429
Marcin Matula <marcin.matula@gmail.com>
2019-02-07 00:25:31 +0100

    download_test_data splited into download_test_data.sh and download_module_data.sh
    download_test_data.sh and download_module_data.sh are bash scripts

---
+++ scripts/download_module_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR=../$1/data
+
+URL=https://sourceforge.net/projects/openap/files/oap_test_data
+DATE=06_02_2019
+ARCHIVE_NAME=oap_test_data_$1_$DATE.tar.gz
+
+if [ "$2" == "preclean" ]; then
+  rm -r $DIR/*
+fi
+
+if [ "$(ls -A $DIR)" ] ; then
+  echo "Dir $DIR must be empty for this operation."
+else
+  wget $URL/$ARCHIVE_NAME
+  tar -xvf $ARCHIVE_NAME -C .. # --strip-components=1
+  rm $ARCHIVE_NAME
+fi
--- scripts/download_test_data.sh
+++ scripts/download_test_data.sh
@@ -1,26 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-DIR1=../oap2dt3d/data
-DIR2=../oap2dt3dFuncTests/data
-DIR3=../oapDeviceTests/data
-DIR4=../oapNeural/data
+MODULES=(oap2dt3d oap2dt3dFuncTests oapDeviceTests oapNeural)
 
-URL=https://sourceforge.net/projects/openap/files/oap_test_data
-DATE=06_02_2019
-ARCHIVE_NAME=oap_test_data_$DATE.tar.gz
-
-if [ "$1" == "preclean" ]; then
-  rm -r $DIR1/*
-  rm -r $DIR2/*
-  rm -r $DIR3/*
-  rm -r $DIR4/*
-fi
-
-if [ "$(ls -A $DIR1)" -o "$(ls -A $DIR2)" -o "$(ls -A $DIR3)" -o "$(ls -A $DIR4)" ] ; then
-  echo "Dirs oap2dt3d/data, oap2dt3dFuncTests/data, oapArnoldiDeviceTests/data oapNeural/data must be empty for this operation."
-else
-  wget $URL/$ARCHIVE_NAME
-  tar -xvf $ARCHIVE_NAME -C .. --strip-components=1
-  rm $ARCHIVE_NAME
-fi
-
+for module in ${MODULES[*]}
+do
+  bash ./download_module_data.sh $module $1
+done
