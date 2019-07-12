484d78b885b662aeced4d127f75a54f90d09e08b
Marcin Matula <marcin.matula@gmail.com>
2019-07-12 12:46:52 +0200

    projects mk - EXTRA -> USER

--- project_albert.mk
+++ project_albert.mk
@@ -1,6 +1,6 @@
 MODE := Debug
 PLATFORM := albert
-EXTRA_CXXOPTIONS :=
-EXTRA_NVCCOPTIONS := -arch=sm_20 --ptxas-options --verbose -maxrregcount=30
+USER_CXXOPTIONS :=
+USER_NVCCOPTIONS := -arch=sm_20 --ptxas-options --verbose -maxrregcount=30
 CXX := g++
 NVCC := nvcc
