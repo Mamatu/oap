3b68743d2d8bee50c90f6a27131671e56e959cff
Marcin Matula <marcin.matula@gmail.com>
2019-07-24 22:44:17 +0200

    oapClassificationTests - changes in args of plotCoords2D

--- oapNeuralDeviceTests/oapClassificationTests.cpp
+++ oapNeuralDeviceTests/oapClassificationTests.cpp
@@ -410,7 +410,7 @@ TEST_F(OapClassificationTests, CircleDataTest)
     }
     while (testError > 0.005 && terrorCount < 10000);
 
-    oap::pyplot::plotCoords2D ("/tmp/plot_plane_xy.py", std::make_tuple(-200, 200, 1), std::make_tuple(-200, 200, 1), getLabel, {"r*", "b*"});
+    oap::pyplot::plotCoords2D ("/tmp/plot_plane_xy.py", std::make_tuple(-5, 5, 0.01), std::make_tuple(-5, 5, 0.01), getLabel, {"r*", "b*"});
   }
 
   oap::cuda::Context::Instance().destroy();
