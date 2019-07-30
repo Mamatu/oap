5f637cde3d23011b55f27dab7cdac2f5c7ed5d8d
Marcin Matula <marcin.matula@gmail.com>
2019-07-30 15:45:06 +0200

    Test for network with layers: {2,3,2,5,4,1}

---
+++ oapDeviceTests/oapNeuralTests_Backpropagation_4HL_3_2_5_4.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2016 - 2019 Marcin Matula
+ *
+ * This file is part of Oap.
+ *
+ * Oap is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Oap is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Oap.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "oapNeuralTests_Api.h"
+
+#include "oapNeuralTests_Data_4HL_3_2_5_4.h"
+
+class OapNeuralTests_Backpropagation_4HL_3_2_5_4 : public testing::Test
+{
+ public:
+
+  virtual void SetUp()
+  {
+    oap::cuda::Context::Instance().create();
+  }
+
+  virtual void TearDown()
+  {
+   oap::cuda::Context::Instance().destroy();
+  }
+};
+
+TEST_F(OapNeuralTests_Backpropagation_4HL_3_2_5_4, Test_1)
+{
+  using namespace oap::Backpropagation_Data_4HL_3_2_5_4;
+
+  auto network = test_api::createNetwork (g_networkInfo);
+  ASSERT_NO_FATAL_FAILURE(test_api::testSteps (network.get(), {g_weights1to2Vec, g_weights2to3Vec, g_weights3to4Vec, g_weights4to5Vec, g_weights5to6Vec}, g_steps, g_idxsToCheck));
+}
