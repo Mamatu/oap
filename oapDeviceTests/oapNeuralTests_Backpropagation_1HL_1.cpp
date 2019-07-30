41700bd7da67852634df3a1b1f0eac41f111eca2
Marcin Matula <marcin.matula@gmail.com>
2019-07-30 21:46:51 +0200

    oapNeuralTests_Backpropagation_10 -> oapDeviceTests/oapNeuralTests_Backpropagation_1HL_1

--- oapDeviceTests/oapNeuralTests_Backpropagation_1HL_1.cpp
+++ oapDeviceTests/oapNeuralTests_Backpropagation_1HL_1.cpp
@@ -19,9 +19,9 @@
 
 #include "oapNeuralTests_Api.h"
 
-#include "oapNeuralTests_Data_10.h"
+#include "oapNeuralTests_Data_1HL_1.h"
 
-class OapNeuralTests_Backpropagation_10 : public testing::Test
+class OapNeuralTests_Backpropagation_1HL_1 : public testing::Test
 {
  public:
 
@@ -36,11 +36,10 @@ class OapNeuralTests_Backpropagation_10 : public testing::Test
   }
 };
 
-TEST_F(OapNeuralTests_Backpropagation_10, Test_1)
+TEST_F(OapNeuralTests_Backpropagation_1HL_1, Test_1)
 {
-  using namespace oap::Backpropagation_Data_10;
+  using namespace oap::Backpropagation_Data_1HL_1::Test_1;
 
   auto network = test_api::createNetwork (g_networkInfo);
   ASSERT_NO_FATAL_FAILURE(test_api::testSteps (network.get(), {g_weights1to2Vec, g_weights2to3Vec}, g_steps, g_idxsToCheck));
 }
-
