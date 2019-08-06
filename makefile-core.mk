74441063b11da55fd59970e84ec7c15fa7c903fd
Marcin Matula <marcin.matula@gmail.com>
2019-08-06 14:43:24 +0200

    makefile-core fix invalid path

--- makefile-core.mk
+++ makefile-core.mk
@@ -8,7 +8,7 @@ OCU_FILES := $(addprefix build/$(MODE)/$(PLATFORM)/,$(notdir $(CU_FILES:.cu=.o))
 INCLUDE_DIRS := -I/usr/local/cuda/include -I/usr/include $(OAP_INCLUDES_PATHS)
 INCLUDE_DIRS += $(INCLUDE_PATHS)
 NVCC_INCLUDE_DIRS := -I/usr/local/cuda/include
-LIBS_DIRS := -L/usr/lib/nvidia-current -L/usr/lib -L/usrs/local/cuda/lib64
+LIBS_DIRS := -L/usr/lib/nvidia-current -L/usr/lib -L/usr/local/cuda/lib64
 LIBS := $(EXTRA_LIBS)
 SANITIZER_LINKING :=
 SANITIZER_COMPILATION :=
