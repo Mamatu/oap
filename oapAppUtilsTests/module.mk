7b5ef9c4b206e446030b59a3abe184be1047edd4
Marcin Matula <marcin.matula@gmail.com>
2019-10-27 20:08:56 +0100

    oap2dt3dFuncTests -> oapAppUtilsFuncTests oap2dt3dTests -> oapAppUtilsTests

--- oapAppUtilsTests/module.mk
+++ oapAppUtilsTests/module.mk
@@ -1,5 +1,5 @@
 include ../project_generic.mk
-TARGET := oap2dt3dTests
+TARGET := oapAppUtilsTests
 INCLUDE_PATHS :=
 EXTRA_LIBS := $(OAP_PATH)/dist/$(MODE)/$(PLATFORM)/lib/liboapAppUtils.so
 EXTRA_CXXOPTIONS := -std=c++11
