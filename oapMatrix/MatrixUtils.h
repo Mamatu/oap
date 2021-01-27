c3f1adaaa57505768de18545f41d2450c465d24b
Marcin Matula <marcin.matula@gmail.com>
2021-01-27 20:49:23 +0100

    Range moved from MatrixUtils into oapRange

--- oapMatrix/MatrixUtils.h
+++ oapMatrix/MatrixUtils.h
@@ -28,6 +28,7 @@
 #include <functional>
 #include "Matrix.h"
 #include "MatrixAPI.h"
+#include "oapRange.h"
 
 namespace matrixUtils
 {
@@ -41,27 +42,6 @@ class OccurencesList : public std::vector<std::pair<uintt, T> > {};
 
 template <typename T>
 class SubArrays : public std::vector<std::pair<T*, uintt> > {};
-
-class Range {
- protected:
-  uintt m_bcolumn;
-  uintt m_columns;
-  uintt m_brow;
-  uintt m_rows;
-
- public:
-  Range(uintt bcolumn, uintt columns, uintt brow, uintt rows);
-
-  virtual ~Range();
-
-  virtual uintt getBColumn() const;
-  virtual uintt getEColumn() const;
-  virtual uintt getColumns() const;
-
-  virtual uintt getBRow() const;
-  virtual uintt getERow() const;
-  virtual uintt getRows() const;
-};
 
 class MatrixRange : public Range {
  protected:
